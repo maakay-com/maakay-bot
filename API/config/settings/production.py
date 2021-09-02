ebfc55270497f7a9c27835b4c79fa4d6253baeda
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-09-02 13:34:55 +0545

    initialized the django and bot structure

---
+++ API/config/settings/production.py
@@ -0,0 +1,16 @@
+import os
+from .base import *  # noqa: F401
+
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT']
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+#### Welcome to Maakay Bot
+
+#### Getting Started
+Clone the repo.
+
+Activate the virtual environment.
+
+Set the environment variable `DJANGO_SETTINGS_MODULE` to `config.settings.development`.
+
+To run django server, navigate to API and use command `python manage.py runserver`.
+
+Discord Bot guide coming soon.
