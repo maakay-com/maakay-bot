a9bba50f81f527431c1337be5f804e7ddd23064d
Hussu <55182298+hussu010@users.noreply.github.com>
2021-10-01 16:22:50 +0545

    built the codebase for dokku release (#56)
    * built the codebase for dokku release

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
+web: gunicorn config.wsgi
 worker: python maakay-bot.py
-web: sh -c 'cd API/ && daphne config.asgi:application --port $PORT --bind 0.0.0.0'
--- core/models/statistics.py
+++ core/models/statistics.py
@@ -8,11 +8,11 @@ class Statistic(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
 
     title = models.CharField(max_length=255)
-    total_balance = models.IntegerField()
-    total_fees_collected = models.IntegerField()
+    total_balance = models.BigIntegerField(default=0)
+    total_fees_collected = models.BigIntegerField(default=0)
 
-    total_servers = models.IntegerField()
-    total_users = models.IntegerField()
+    total_servers = models.IntegerField(default=0)
+    total_users = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Balance: {self.total_balance}; Servers: {self.total_servers}; Users: {self.total_users}"
