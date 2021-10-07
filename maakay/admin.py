e362c87f92d1d35a817574251ff709c102b2e413
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-10-07 14:49:49 +0545

    added tip table on admin panel

--- maakay/admin.py
+++ maakay/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 from .models.challenges import Challenge
 from .models.tournaments import Tournament
-from .models.users import MaakayUser
+from .models.users import MaakayUser, UserTip
 
 
 admin.site.register(Challenge)
 admin.site.register(MaakayUser)
 admin.site.register(Tournament)
+admin.site.register(UserTip)
