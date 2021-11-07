c66b57d77813b91a16c7df0782f2f6b3a2266f05
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-11-07 17:17:48 +0545

    updated Guild model

--- core/models/guild.py
+++ core/models/guild.py
@@ -9,16 +9,13 @@ class Guild(models.Model):
 
     guild_id = models.CharField(max_length=255)
 
-    trade_channel_id = models.CharField(max_length=255, blank=True, null=True)
-    dispute_channel_id = models.CharField(max_length=255, blank=True, null=True)
     tournament_channel_id = models.CharField(max_length=255, blank=True, null=True)
-
-    admin_role_id = models.CharField(max_length=255, blank=True, null=True)
     manager_role_id = models.CharField(max_length=255, blank=True, null=True)
+
     withdrawal_address = models.CharField(max_length=64, blank=True, null=True)
 
-    total_fee_collected = models.IntegerField(default=0)
-    guild_balance = models.IntegerField(default=0)
+    total_fee_collected = models.BigIntegerField(default=0)
+    guild_balance = models.BigIntegerField(default=0)
 
     def __str__(self):
         return f"Server: {self.guild_id}; Fees: {self.total_fee_collected}"
